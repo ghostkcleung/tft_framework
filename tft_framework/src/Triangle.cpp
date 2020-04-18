d7f2ab480c36c4913303dd2a114ddeeab107d61a
ghostkcleung <37092163+ghostkcleung@users.noreply.github.com>
2020-04-19 06:23:20 +0800

    update

--- tft_framework/src/Triangle.cpp
+++ tft_framework/src/Triangle.cpp
@@ -19,8 +19,16 @@ void Triangle::setP2 ( Point p2 ) {
 	this -> p2 = p2;
 }
 
+void Triangle::setP2 ( int16_t x, int16_t y ) {
+	this -> p2.setPoint ( x, y );
+}
+
 void Triangle::setP3 ( Point p3 ) {
 	this -> p3 = p3 ;
+}
+
+void Triangle::setP3 ( int16_t x, int16_t y ) {
+	this -> p3.setPoint ( x, y );
 }
 
 void Triangle::move ( double direction, double distance ){
--- tft_framework/src/Triangle.h
+++ tft_framework/src/Triangle.h
@@ -10,7 +10,9 @@ public:
     void draw(Screen* scr);
     void fill(Screen* scr);
 	void setP2 ( Point p2 );
+	void setP2 ( int16_t x, int16_t y );
 	void setP3 ( Point p3 );
+	void setP3 ( int16_t x, int16_t y );
 	Point getP2 ( );
 	Point getP3 ( );
 	void move ( double direction, double distance );
