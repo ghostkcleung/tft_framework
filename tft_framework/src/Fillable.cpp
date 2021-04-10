4c1b4ebf04c1fccc70103de20f7d08482d3c0618
ghostkcleung <37092163+ghostkcleung@users.noreply.github.com>
2021-04-10 21:16:00 +0800

    Delete tft_framework directory

--- tft_framework/src/Fillable.cpp
+++
@@ -1,7 +0,0 @@
-#include "tft_framework.h"
-
-using namespace tft_framework;
-
-void Fillable::fill (Screen* scr) {
-	scr -> fillShape ( this ) ;
-}
--- tft_framework/src/Shape.cpp
+++
@@ -1,7 +0,0 @@
-#include "tft_framework.h"
-
-using namespace tft_framework;
-
-void Shape::draw (Screen* scr) {
-	scr -> drawShape ( this ) ;
-}
