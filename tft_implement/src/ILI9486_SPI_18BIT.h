4bb34bb15e32894f6d85f959269f69864629f3ce
ghostkcleung <37092163+ghostkcleung@users.noreply.github.com>
2021-05-21 21:54:17 +0800

    Add files via upload

---
+++ tft_implement/src/ILI9486_SPI_18BIT.h
@@ -0,0 +1,43 @@
+#ifdef ESP32
+
+#include <SPI.h>
+
+#ifndef ILI9486_SPI_18BIT_H
+#define ILI9486_SPI_18BIT_H
+
+#include <ILI9486.h>
+
+
+namespace tft_framework {
+
+class ILI9486_SPI_18BIT:public ILI9486
+{
+
+private:
+	uint8_t cs, dc;
+	SPIClass* spi;
+
+protected:
+
+	void writeComs ( uint8_t com, uint8_t *datas, unsigned int count );
+	void setWindow ( Rectangle* r );
+
+public:
+
+	ILI9486_SPI_18BIT (SPIClass* spi, uint8_t cs, uint8_t dc)
+		:ILI9486(480,320),spi(spi),cs(cs),dc(dc){};
+
+	ILI9486_SPI_18BIT (uint8_t dc);
+
+    void init ( );
+    void fillRect(Rectangle* r);
+    void setRotate(uint8_t rotate);
+
+    void fillShape(BufferScreen* buf);
+	void drawShape(Dot *d);
+	~ILI9486_SPI_18BIT();
+};
+
+}
+#endif
+#endif
