7eea8ffb34d40ce41412e0159032397b39681ea6
ghostkcleung <37092163+ghostkcleung@users.noreply.github.com>
2021-05-21 22:04:00 +0800

    Add files via upload

---
+++ tft_implement/examples/drawArduinoLogo/drawArduinoLogo.ino
@@ -0,0 +1,91 @@
+#ifdef ESP32
+  #include <ILI9486_SPI_18BIT.h>
+#else
+  #include <ILI9486_Parallel.h>
+#endif
+
+#include "splashScreenData.h"
+
+using namespace tft_framework;
+
+Screen* scr;
+
+void setup()
+{
+    #ifdef ESP32
+		uint8_t bl = 32;
+		pinMode ( bl, OUTPUT ) ;
+		digitalWrite ( bl, HIGH ) ;
+
+		uint8_t cs = SS,
+			dc = 27;
+
+		// scr = new ILI9486_SPI_18BIT ( 27 ) ;
+
+		uint32_t clock = 27000000 ;
+
+		SPIClass* spi = new SPIClass(VSPI);
+		spi = new SPIClass(VSPI);
+		spi->begin(SCK, MISO, MOSI, cs);
+
+		spi -> beginTransaction(SPISettings(clock, MSBFIRST, SPI_MODE0));
+
+		scr = new ILI9486_SPI_18BIT ( spi, cs, dc ) ;
+    #else
+		scr = new ILI9486_Parallel ( ) ;
+    #endif
+
+    scr -> init ( ) ;
+    scr -> setColor ( 0xFFFF ) ;
+
+    scr -> clear ( ) ;
+    Line l;
+    l.setRGB ( 0x979C ) ;
+
+    int16_t x = 0, y = 281;
+
+    for ( int i = 0; i < 2570; i ++ )
+    {
+        uint16_t len = pgm_read_word_near(splashScreenData + i);
+
+        if ( i % 2 )
+        {
+            l.setPoint ( x+60, y ) ;
+
+            if ( x + len < 360 )
+            {
+                x += len;
+                l.setEndPoint ( x+60, y ) ;
+                l.draw ( scr ) ;
+            }
+            else
+            {
+                l.setEndPoint ( 419, y -- ) ;
+                l.draw ( scr ) ;
+
+                x = len - ( 360 - x ) ;
+                l.setPoint ( 60, y );
+                l.setEndPoint ( x+60, y );
+                l.draw ( scr ) ;
+            }
+        }
+        else
+        {
+            if ( x + len < 360 )
+            {
+                x += len ;
+            }
+            else
+            {
+                y -= len / 360 + 1 ;
+                len %= 360;
+                x = len - ( 360 - x ) ;
+            }
+        }
+    }
+}
+
+void loop ( )
+{
+    delay ( 1000000 ) ;
+}
