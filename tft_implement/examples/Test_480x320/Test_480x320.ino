63b83a95d80e6509d6cdce887ab2ab52a9995c1c
ghostkcleung <37092163+ghostkcleung@users.noreply.github.com>
2021-05-21 21:55:03 +0800

    Add files via upload

---
+++ tft_implement/examples/Test_480x320/Test_480x320.ino
@@ -0,0 +1,80 @@
+#ifdef ESP32
+  #include <ILI9486_SPI_18BIT.h>
+#else
+  #include <ILI9486_Parallel.h>
+#endif
+
+using namespace tft_framework;
+Screen* scr;
+
+#include "hello.h"
+#include "fillScreenTest.h"
+#include "drawDotTest.h"
+#include "drawLineTest.h"
+#include "drawRectTest.h"
+#include "fillRectTest.h"
+#include "drawTriangleTest.h"
+#include "fillTriangleTest.h"
+#include "drawCircleTest.h"
+#include "fillCircleTest.h"
+#include "drawEllipseTest.h"
+#include "fillEllipseTest.h"
+#include "allColorTest.h"
+#include "bufferTest.h"
+#include "testDrawFont.h"
+#include "testPrint.h"
+
+void setup() {
+    randomSeed(analogRead(A0));
+
+    #ifdef ESP32
+		uint8_t bl = 32;
+		pinMode ( bl, OUTPUT ) ;
+		digitalWrite ( bl, HIGH ) ;
+
+		// scr = new ILI9486_SPI_18BIT ( 27 ) ;
+
+		uint8_t cs = SS,
+			dc = 27;
+
+		uint32_t clock = 27000000 ;
+
+		SPIClass* spi = new SPIClass(VSPI);
+		spi = new SPIClass(VSPI);
+		spi->begin(SCK, MISO, MOSI, cs);
+
+		spi -> beginTransaction(SPISettings(clock, MSBFIRST, SPI_MODE0));
+
+		scr = new ILI9486_SPI_18BIT ( spi, cs, dc ) ;
+    #else
+		scr = new ILI9486_Parallel ( ) ;
+    #endif
+    
+    scr -> init();
+    scr -> setFontScale ( 2 ) ;
+    scr -> setFontPadding ( 1 ) ;
+    scr -> setPrintBuffer ( true ) ;
+    scr -> clear ( ) ;
+}
+
+void loop ( ) {
+
+  hello();
+  fillScreenTest ( );
+  drawDotTest ( );
+  drawLineTest ( );
+  drawRectTest ( );
+  fillRectTest ( );
+  drawTriangleTest ( );
+  fillTriangleTest ( );
+  drawCircleTest ( );
+  fillCircleTest ( );
+  drawEllipseTest ( );
+  fillEllipseTest ( );
+  allColorTest ( );
+  bufferTest ( ) ;
+  testDrawFont ( ) ;
+  testPrint ( ) ;
+
+  delay ( 1000000 );
+}
